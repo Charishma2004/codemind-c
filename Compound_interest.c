53b7b9d1bf19ab56bd18107de569ee216fad9cc5
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-04-20 09:55:28 +0530

    # include <stdio.h> # include <math.h> int main() {     double p,r,t,ci;     scanf("%lf",&p);     scanf("%lf",&r);     scanf("%lf",&t);     ci=p*pow((1+r/100),t);     printf("%0.2lf",ci);     return 0; }

---
+++ Compound_interest.c
@@ -0,0 +1,13 @@
+
+# include <stdio.h>
+# include <math.h>
+int main()
+{
+    double p,r,t,ci;
+    scanf("%lf",&p);
+    scanf("%lf",&r);
+    scanf("%lf",&t);
+    ci=p*pow((1+r/100),t);
+    printf("%0.2lf",ci);
+    return 0;
+}

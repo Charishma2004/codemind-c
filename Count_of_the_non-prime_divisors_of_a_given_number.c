d108b171a3be2f59232d16e704706e0470eef0cc
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-29 19:55:11 +0530

    #include<stdio.h> int is_prime(int n) {     int i,c=0;     for(i=2;i<=n/2;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==0)     {         return 1;     }     return 0; } int main() {     int n,count=2;     scanf("%d",&n);     for(int i=2;i<=n/2;i++)     {         if(n%i==0)         {             if(is_prime(i))             {
                    i++;
                    continue;
                }
                else
                {
                    count++;
                }
            }
        }
        printf("%d",count);
    }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,25 +1,39 @@
 #include<stdio.h>
-int main()
+int is_prime(int n)
 {
-    int n,i,c=0,s=0,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int i,c=0;
+    for(i=2;i<=n/2;i++)
     {
         if(n%i==0)
         {
-            c=0;
-        for(j=1;j<=i;j++)
-        {
-            if(i%j==0)
-            {
-                c++;
-            }
-        }
-        if(c!=2)
-        {
-            s++;
-        }
+            c++;
         }
     }
-    printf("%d",s);
+    if(c==0)
+    {
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int n,count=2;
+    scanf("%d",&n);
+    for(int i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            if(is_prime(i))
+            {
+                
+                i++;
+                continue;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
 }

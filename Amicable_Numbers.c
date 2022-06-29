c38aba1892c92a262e5c869c9717920bff9adc11
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-29 22:52:00 +0530

    #include<stdio.h> int main() {     long long int a,b,sum=0,i;     scanf("%lld%lld",&a,&b);     for(i=1;i<a;i++)     {         if(a%i==0)         sum=sum+i;     }     if(sum==b)     printf("Amicable");     else     printf("Not Amicable");     return 0; }

--- Amicable_Numbers.c
+++ Amicable_Numbers.c
@@ -1,29 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,j,s=0,c=0,k;
-    scanf("%d",&a);
-    scanf("%d",&b);
+    long long int a,b,sum=0,i;
+    scanf("%lld%lld",&a,&b);
     for(i=1;i<a;i++)
     {
         if(a%i==0)
-        {
-            s=s+i;
-        }
+        sum=sum+i;
     }
-    for(j=1;j<b;j++)
-    {
-        if(b%j==0)
-        {
-            c=c+j;
-        }
-    }
-    if((a==c)&&(s==b))
-    {
-        printf("Amicable");
-    }
+    if(sum==b)
+    printf("Amicable");
     else
-    {
-        printf("Not Amicable");
-    }
+    printf("Not Amicable");
+    return 0;
 }

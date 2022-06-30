46c26ed48e14f5cf9a11ee60f6dc41c132e400c3
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 20:09:42 +0530

    #include<stdio.h> int counting(int n,int *b,int len) {     int m;     for(m=0;m<len;m++)     {         if(b[m]==n)         return 0;     }     return 1; } int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m,flag=0;     scanf("%d",&m);     int j=0,k=0,b[n];     for(i=0;i<n;i++)     {         int count=1;         //int flag=0;         if(counting(a[i],b,n))         {             b[k]=a[i];             k++;             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     count++;                 }             }         }         if(count==m)         {             printf("%d ",a[i]);             flag++;         }
        }
        if(flag==0)
        printf("-1");
        return 0;
    }

--- N_times_repeated_elements.c
+++ N_times_repeated_elements.c
@@ -1,35 +1,50 @@
 #include<stdio.h>
+int counting(int n,int *b,int len)
+{
+    int m;
+    for(m=0;m<len;m++)
+    {
+        if(b[m]==n)
+        return 0;
+    }
+    return 1;
+}
 int main()
 {
-    int n,arr[100],i,c,k,m=0,j;
+    int n;
     scanf("%d",&n);
+    int a[n],i;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
-    scanf("%d",&k);
+    int m,flag=0;
+    scanf("%d",&m);
+    int j=0,k=0,b[n];
     for(i=0;i<n;i++)
     {
-        c=1;
-        if(arr[i]!=-1)
+        int count=1;
+        //int flag=0;
+        if(counting(a[i],b,n))
         {
-            for(j=0;j<n;j++)
+            b[k]=a[i];
+            k++;
+            for(j=i+1;j<n;j++)
             {
-                if(arr[i]==arr[j]&&i!=j)
+                if(a[i]==a[j])
                 {
-                    c++;
-                    arr[j]=-1;
+                    count++;
                 }
             }
-            if(c==k)
-            {
-                printf("%d ",arr[i]);
-                m++;
-            }
         }
+        if(count==m)
+        {
+            printf("%d ",a[i]);
+            flag++;
+        }
+        
     }
-    if(m==0)
-    {
-        printf("-1");
-    }
+    if(flag==0)
+    printf("-1");
+    return 0;
 }

c6d86615a54324ae3be61456b0121df7e7c50fc5
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 23:50:00 +0530

    #include<stdio.h> #include<string.h> int main() {     int i,n,c=0,s=0;     char str[10000];     scanf("%[^ ]s",str);     n=strlen(str);     for(i=0;i<n;i++)     {         if(str[i]==' ')         s++;     }     printf("%d",(n-s)); }

---
+++ without_spaces_number_of_characters.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,n,c=0,s=0;
+    char str[10000];
+    scanf("%[^
+]s",str);
+    n=strlen(str);
+    for(i=0;i<n;i++)
+    {
+        if(str[i]==' ')
+        s++;
+    }
+    printf("%d",(n-s));
+}

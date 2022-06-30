aa3d0eaa794b9591c0ef601d0039722338e7af6f
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 20:27:54 +0530

    #include<stdio.h> int main() {     int i,n,a[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<n-1;i++)     {         if(a[i]%2!=0 && a[i+1]%2!=0 && a[i-1]%2!=0)         {             c++;         }     }     printf("%d",c); }

--- count_the_add_numbers_inbetween_odd_numbers.c
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,j,c=0;
+    int i,n,a[100],c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=1;i<n-1;i++)
     {
-        c=0;
-        for(j=1;j<n-1;j++)
+        if(a[i]%2!=0 && a[i+1]%2!=0 && a[i-1]%2!=0)
         {
-            if(arr[j]%2 &&arr[j+1]%2 &&arr[j-1]%2)
-            {
-                c++;
-            }
+            c++;
         }
     }
     printf("%d",c);

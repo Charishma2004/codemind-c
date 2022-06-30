2ffb802e60fa1101f35aba48708aa9a1f96c6bf6
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 05:42:15 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     //cout<<n;     int s=sqrt(n);     //cout<<s;     if(n==s*s)cout<<"True";     else cout<<"False"; }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,15 +1,12 @@
-#include<stdio.h>
-#include<math.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
-    int n,s;
-    scanf("%d",&n);
-    s=(int)sqrt(n);
-    if(s*s==n)
-      printf("True");
-    else
-      printf("False");
-    
-    return 0;
-     
+    int n;
+    cin>>n;
+    //cout<<n;
+    int s=sqrt(n);
+    //cout<<s;
+    if(n==s*s)cout<<"True";
+    else cout<<"False";
 }

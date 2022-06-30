705e08e0acdd457df44245c85f544c3eff7e6fca
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 10:51:56 +0530

    #include<stdio.h> int pal(int); int pal(int a) {     int num,rem,rev=0;     num=a;     while(a!=0)     {         rem=a%10;         rev=rev*10+rem;         a/=10;     }     return rev; } int main() {     int n,a[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         printf("%d ",pal(a[i]));     } }

--- Reverse_game.c
+++ Reverse_game.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
+int pal(int);
+int pal(int a)
+{
+    int num,rem,rev=0;
+    num=a;
+    while(a!=0)
+    {
+        rem=a%10;
+        rev=rev*10+rem;
+        a/=10;
+    }
+    return rev;
+}
 int main()
 {
-    int n,arr[100],i,rev,d,t;
+    int n,a[100],i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-        rev=0;
-        t=arr[i];
-        while(arr[i]!=0)
-        {
-            d=arr[i]%10;
-            arr[i]=arr[i]/10;
-            rev=rev*10+d;
-        }
-        printf("%d ",rev);
+        printf("%d ",pal(a[i]));
     }
 }

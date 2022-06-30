603256412ee83fe78ac33abc85fe0ce58afa04dc
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 20:26:28 +0530

    #include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-2;i++)     {         if(arr[i]%2==0 && arr[i+2]%2 || arr[i]%2 && arr[i+2]%2==0)         {             c++;         }     }     printf("%d",c); }

--- count_numbers_between_even_and_odd_numbers_or_vice_versa.c
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -1,23 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,j,c=0;
+    int n,c=0;
     scanf("%d",&n);
+    int arr[n],i;
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=1;i<n-1;i++)
+    for(i=0;i<n-2;i++)
     {
-        c=0;
-        for(j=1;j<n-1;j++)
+        if(arr[i]%2==0 && arr[i+2]%2 || arr[i]%2 && arr[i+2]%2==0)
         {
-            if(arr[j+1]%2 && arr[j-1]%2==0 ||arr[j+1]%2==0 &&arr[j-1]%2)
-            {
-                c++;
-            }
+            c++;
         }
-        
     }
     printf("%d",c);
 }

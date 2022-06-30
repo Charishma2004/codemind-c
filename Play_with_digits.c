49a1d185fd9d7ec4fc2e4687f967abfa4f034cd6
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 10:53:16 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[100],i,sum=0,count=0,temp,r;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         temp=a[i];         while(temp>0)         {             r=temp%10;             sum=sum+r;             temp=temp/10;         }     }     printf("%d",sum); }

--- Play_with_digits.c
+++ Play_with_digits.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,s=0,d;
+    int n;
     scanf("%d",&n);
+    int a[100],i,sum=0,count=0,temp,r;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-        while(arr[i]!=0)
+        temp=a[i];
+        while(temp>0)
         {
-            d=arr[i]%10;
-            arr[i]=arr[i]/10;
-            s=s+d;
+            r=temp%10;
+            sum=sum+r;
+            temp=temp/10;
         }
     }
-    printf("%d",s);
+    printf("%d",sum);
 }

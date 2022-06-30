3eb06c10f8edbe522ec4756f375169d07c29f6b0
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 21:13:37 +0530

    #include<stdio.h> int main(){     int a,b,arr[10000],i,n,sum=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a||arr[i]>b)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

--- sum_of_elements_not_between_A_and_B.c
+++ sum_of_elements_not_between_A_and_B.c
@@ -1,22 +1,16 @@
 #include<stdio.h>
-int main()
-{
-    int n;
+int main(){
+    int a,b,arr[10000],i,n,sum=0,c=0;
     scanf("%d",&n);
-    int arr[n],i,sum=0;
     for(i=0;i<n;i++)
-    {
-       scanf("%d",&arr[i]);
-    }
-     int a,b;
+    scanf("%d",&arr[i]);
     scanf("%d%d",&a,&b);
     for(i=0;i<n;i++)
     {
-        if(arr[i]<a || arr[i]>b)
+        if(arr[i]<a||arr[i]>b)
         {
-            sum+=arr[i];
+            sum=sum+arr[i];
         }
     }
-   printf("%d",sum);
-    
+    printf("%d",sum);
 }

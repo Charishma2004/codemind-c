e6c8d133a90f1ca90ef89eae8f00e01b057144ea
Charishma2004 <98752245+Charishma2004@users.noreply.github.com>
2022-06-30 11:32:08 +0530

    #include<stdio.h> int main() {     int i,n,a[100],s=0,k=0,j,b[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {             if(a[i]%2==0)             {                 b[k]=a[i];                 k++;             }     }     for(i=0;i<k;i++)     {         for(j=0;j<k;j++)         {             if(b[i]==b[j] && i!=j)             {                 b[j]=-1;             }         }     }     for(i=0;i<k;i++)     {         if(b[i]!=-1)         {             s=s+b[i];         }     }
        printf("%d",s);
    }

--- unique_even_elements_sum.c
+++ unique_even_elements_sum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,a[100],s=0,k=0,j,b[100],c=0;
+    int i,n,a[100],s=0,k=0,j,b[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -9,17 +9,17 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2==0)
-        {
-            b[k]=a[i];
-            k++;
-        }
+            if(a[i]%2==0)
+            {
+                b[k]=a[i];
+                k++;
+            }
     }
     for(i=0;i<k;i++)
     {
         for(j=0;j<k;j++)
         {
-            if(b[i]==b[j] &&i!=j)
+            if(b[i]==b[j] && i!=j)
             {
                 b[j]=-1;
             }
@@ -29,8 +29,9 @@ int main()
     {
         if(b[i]!=-1)
         {
-            c=c+b[i];
+            s=s+b[i];
         }
     }
-    printf("%d",c);
+    
+    printf("%d",s);
 }
